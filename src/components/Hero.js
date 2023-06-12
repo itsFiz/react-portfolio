67b7bce82ee75417886724ae98dc68acfc4efe10
PrantoshB <pbiswas1007@gmail.com>
2023-06-12 20:17:58 +0530

    Customize pages with CSS

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ import './Hero.css'
 const Hero = () => {
 
     return (
-        <div className='hero'>
+        <div className='hero' id='home'>
             <h2 className='title'>Prantosh Biswas</h2>
             <h3 className='tagline'>Full-Stack Developer</h3>
             <p>Hello there! I'm Prantosh, a full stack developer passionate about crafting exceptional digital experiences. With a keyboard as my paintbrush and lines of code as my canvas, I bring ideas to life in the virtual realm. Let's join forces and bring your digital dreams to fruition. Get in touch, and let's embark on this exciting journey together!</p>
@@ -13,14 +13,17 @@ const Hero = () => {
                 <button className="lets-talk">Let's Talk</button>
             </div>
             <div className="social-icons">
+                <a href="https://github.com/PrantoshB" target="_blank">
+                    <i class="fa-brands fa-github"></i>
+                </a>
                 <a href="https://www.linkedin.com/in/prantosh" target="_blank">
                     <i class="fa-brands fa-linkedin-in"></i>
                 </a>
+                <a href="https://wellfound.com/u/prantosh-biswas">
+                <i class="fab fa-angellist"></i>
+                </a>
                 <a href="https://www.twitter.com/prantalks" target="_blank">
                     <i class="fa-brands fa-twitter"></i>
-                </a>
-                <a href="https://github.com/PrantoshB" target="_blank">
-                    <i class="fa-brands fa-github"></i>
                 </a>
             </div>
         </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,10 +7,27 @@ const NavBar = () => {
             <nav>
                 <h1>Prantosh.</h1>
                 <ul className='menu'>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Projects</li>
-                    <li>Contact</li>
+
+                    <li>
+                        <a href="#home"> 
+                        Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#about">
+                            About
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#projects">
+                            Projects
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#contact">
+                            Contact
+                        </a>
+                    </li>
                 </ul>
             </nav>
         </>
